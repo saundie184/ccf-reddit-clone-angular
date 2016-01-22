1023b864d7b01fca2d50d0e5f31e04337343cbf0
Saundie Weiss <saundieweiss@gmail.com>
2016-01-22 10:35:45 -0700

    Added a form for submitting new posts and implemented form validations

---
+++ app.js
@@ -0,0 +1,11 @@
+angular.module('redditClone', [])
+  .controller('FormController', [function() {
+    var vm = this;
+
+    // vm.classTitle = {
+    //   "has-error": newPost.title.$invalid && newPost.title.$dirty,
+    //   "has-success": newPost.title.$valid
+    // };
+
+
+  }]);
